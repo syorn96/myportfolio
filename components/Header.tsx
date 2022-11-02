24962ba14fe002fe0a322c68fb1c79ba7f210fe0
syorn96 <syorn2014@gmail.com>
2022-11-02 11:44:06 -0700

    header complete

---
+++ components/Header.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { SocialIcon } from 'react-social-icons';
+import { motion } from 'framer-motion'
+
+type Props = {}
+
+const Header = (props: Props) => {
+  return (
+    <header className='sticky top-0 p-5 flex-tems-start flex justify-between max-w-7xl mx-auto'>
+        <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x:0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5
+        }} 
+        className='flex flex-row items-center'>
+          <SocialIcon 
+          url='https://www.linkedin.com/in/stephenyorn/' 
+          fgColor='gray'
+          bgColor='transparent'
+          />
+          <SocialIcon 
+          url='https://github.com/syorn96' 
+          fgColor='gray'
+          bgColor='transparent'
+          />
+        </motion.div>
+        <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x:0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5
+        }}  
+        className='flex flex-row items-center text-gray-300 cursor-pointer'>
+          <SocialIcon
+          className='cursor-pointer' 
+          network='email'
+          fgColor='gray'
+          bgColor='transparent'
+          />
+          <p className='uppercase hidden md:inline-flex text-sm text-gray-500'>Contact me</p>
+        </motion.div>
+    </header>
+  )
+}
+
+export default Header
