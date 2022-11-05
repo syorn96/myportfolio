12e76e8cd9dae2ce12eb891c06a47c3649193c36
syorn96 <syorn2014@gmail.com>
2022-11-04 18:21:27 -0700

    updates skills. include names

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -3,12 +3,12 @@ import {motion} from 'framer-motion';
 
 type Props = {
     directionLeft?: boolean;
-    skillImage?: string;
+    skillImage?: Array<string>;
 };
 
 export default function Skill({ directionLeft, skillImage}: Props) {
   return (
-    <div className='group relative flex cursor-pointer'>
+    <div className='group relative flex cursor-pointer items-center justify-center'>
         <motion.img
         initial={{
             x: directionLeft ? -200 : 200,
@@ -16,9 +16,10 @@ export default function Skill({ directionLeft, skillImage}: Props) {
         }}
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
-        src={`${skillImage}`}
+        src={`${skillImage.url}`}
         className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale' 
         />
+        <div className='items-center absolute top-0 my-20 py-3.5 justify-center '>{skillImage.name}</div>
     </div>
   )
 }
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -2,7 +2,55 @@ import React from 'react';
 import {motion} from 'framer-motion';
 import Skill from './Skill'
 
-const iconArray = ['https://img.icons8.com/color/512/javascript--v1.png','https://img.icons8.com/color/512/html-5.png','https://img.icons8.com/color/512/git.png','https://img.icons8.com/color/512/github.png','https://img.icons8.com/color/512/nodejs.png','https://img.icons8.com/color/512/my-sql.png','https://img.icons8.com/color/512/postgreesql.png','https://img.icons8.com/color/512/mongodb.png','https://img.icons8.com/color/512/react-native.png','https://img.icons8.com/color/512/python.png','https://img.icons8.com/color/512/tailwind_css.png','https://img.icons8.com/color/512/bootstrap.png']
+const iconArray = [
+    {
+        url: 'https://img.icons8.com/color/512/javascript--v1.png',
+        name: 'Javascript'
+    },
+    {
+        url: 'https://img.icons8.com/color/512/html-5.png',
+        name: 'HTML'
+    },
+    {
+        url:'https://img.icons8.com/color/512/git.png',
+        name: 'GIT'
+    },
+    {
+        url: 'https://img.icons8.com/color/512/github.png',
+        name: 'GitHub'
+    },
+    {
+        url: 'https://img.icons8.com/color/512/nodejs.png',
+        name: 'NODE'
+    },
+    {
+        url:'https://img.icons8.com/color/512/my-sql.png',
+        name: 'SQL'
+    },
+    {
+        url: 'https://img.icons8.com/color/512/postgreesql.png',
+        name: 'Postgresql'
+    },
+    {
+        url: 'https://img.icons8.com/color/512/mongodb.png',
+        name: 'MongoDB'
+    },
+    {
+        url: 'https://img.icons8.com/color/512/react-native.png',
+        name: 'React.js'
+    },
+    {
+        url: 'https://img.icons8.com/color/512/python.png',
+        name: 'Python'
+    },
+    {
+        url: 'https://img.icons8.com/color/512/tailwind_css.png',
+        name: 'Tailwind'
+    },
+    {
+        url: 'https://img.icons8.com/color/512/bootstrap.png',
+        name: 'Bootstrap'
+    }]
 
 type Props = {}
 
